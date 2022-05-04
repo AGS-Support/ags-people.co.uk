b09090985d5e023f7d06d779d972339db7f38b00
Chris Forte <chris@indieridge.com>
2022-05-04 11:13:18 +0100

    add archive page and pagination

---
+++ src/templates/post.js
@@ -0,0 +1,92 @@
+import React from "react"
+import parse from "html-react-parser"
+import { Link, graphql } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const Post = ({ data: { previous, next, post } }) => {
+  const featuredImage = {
+    data: post.featuredImage?.node?.localFile?.childImageSharp?.gatsbyImageData,
+    alt: post.featuredImage?.node?.alt || ``,
+  }
+  return (
+    <>
+      {featuredImage?.data && (
+        <GatsbyImage
+          image={featuredImage.data}
+          alt={featuredImage.alt}
+          style={{ marginBottom: 50 }}
+        />
+      )}
+      <div>{parse(post.content)}</div>
+      <nav className="blog-post-nav">
+        <ul
+          style={{
+            display: `flex`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+            listStyle: `none`,
+            padding: 0,
+          }}
+        >
+          <li>
+            {previous && (
+              <Link to={previous.uri} rel="prev">
+                ← {parse(previous.title)}
+              </Link>
+            )}
+          </li>
+
+          <li>
+            {next && (
+              <Link to={next.uri} rel="next">
+                {parse(next.title)} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+export default Post
+
+export const pageQuery = graphql`
+  query BlogPostById(
+    $id: String!
+    $previousPostId: String
+    $nextPostId: String
+  ) {
+    post: wpPost(id: { eq: $id }) {
+      id
+      excerpt
+      content
+      title
+      date(formatString: "MMMM DD, YYYY")
+      featuredImage {
+        node {
+          altText
+          localFile {
+            childImageSharp {
+              gatsbyImageData(
+                quality: 100
+                placeholder: TRACED_SVG
+                layout: FULL_WIDTH
+              )
+            }
+          }
+        }
+      }
+    }
+    previous: wpPost(id: { eq: $previousPostId }) {
+      uri
+      title
+    }
+    next: wpPost(id: { eq: $nextPostId }) {
+      uri
+      title
+    }
+  }
+`
