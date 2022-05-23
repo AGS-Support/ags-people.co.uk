c19c1ba2524a4099dd75628dd8fc2aa095010e9f
Chris Forte <chris@indieridge.com>
2022-05-23 11:31:59 +0100

    remove quicklinks from whoe-we-are

--- src/pages/who-we-are.js
+++ src/pages/who-we-are.js
@@ -3,10 +3,6 @@ import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
-import Banner from "../components/Home/Banner"
-import Quicklinks from "../components/Home/Quicklinks"
-import Service from "../components/Home/Service"
-import Testimonials from "../components/Home/Testimonials"
 
 const WhoWeArePage = ({ data }) => {
   return "hello"
