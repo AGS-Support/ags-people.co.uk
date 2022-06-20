6067a6cb7703db7f3194797c984b966ca2b0d68b
Chris Forte <chris@indieridge.com>
2022-06-20 10:54:28 +0100

    remove background from logo

--- src/components/molecules/LogoGrid/LogoGrid.jsx
+++ src/components/molecules/LogoGrid/LogoGrid.jsx
@@ -14,7 +14,7 @@ const LogoGrid = ({ columns, mobileColumns }) => {
           className={`brands__item ${
             index > 8 ? "hidden md:inline" : "visible"
           }`}
-          style={{ minHeight: "75px", background: "#f2f2f2" }}
+          style={{ minHeight: "75px" }}
         >
           <span>
             <GatsbyImage image={logoImage} alt="Customer Logo" />
